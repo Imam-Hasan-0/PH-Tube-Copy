978609a19c83f5d5b2755f0c5e439ff116a86387
Imam-Hasan-0 <ihasan063@gmail.com>
2024-10-05 23:37:55 +0600

    practice with copy design

---
+++ video.js
@@ -0,0 +1,25 @@
+// load categories
+const loadCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error))
+}
+
+// display categories
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories")
+
+    categories.forEach((item) => {
+        console.log(item);
+        // create a button
+        const button = document.createElement("button");
+        button.classList = "btn";
+        button.innerText = item.category;
+
+        // add button to category container
+        categoryContainer.append(button);
+    });
+}
+
+loadCategories()
