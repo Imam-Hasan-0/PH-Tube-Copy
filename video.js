3148db620d5d359babd981101a5a1e5495176242
Imam-Hasan-0 <ihasan063@gmail.com>
2024-10-06 21:06:17 +0600

    complete 80% work

--- video.js
+++ video.js
@@ -1,10 +1,34 @@
 // load categories
 const loadCategories = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then((res) => res.json())
-    .then((data) => displayCategories(data.categories))
-    .catch((error) => console.log(error))
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
+        .catch((error) => console.log(error))
 };
+
+// remove active class function
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName("category-btn")
+    for(let btn of buttons) {
+        btn.classList.remove("active")
+    }
+}
+
+// video category load
+const loadCategoriesVideos = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            // remove active button
+            removeActiveClass()
+
+            // add active button
+            const activeBtn = document.getElementById(`btn-${id}`)
+            activeBtn.classList.add("active")
+            displayVideos(data.category)
+        })
+        .catch((error) => console.log(error))
+}
 
 // display categories
 const displayCategories = (categories) => {
@@ -13,17 +37,18 @@ const displayCategories = (categories) => {
     categories.forEach((item) => {
         console.log(item);
         // create a button
-        const button = document.createElement("button");
-        button.classList = "btn";
-        button.innerText = item.category;
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML = `
+        <button id="btn-${item.category_id}" onclick="loadCategoriesVideos(${item.category_id})" class="btn category-btn" >${item.category}</button>
+        `
 
         // add button to category container
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     });
 }
 
 // time string setup
-function getTimeString(time){
+function getTimeString(time) {
     const hour = parseInt(time / 3600);
     let rSecond = parseInt(time % 3600);
     const minute = parseInt(rSecond / 60);
@@ -34,24 +59,35 @@ function getTimeString(time){
 // load videos
 const loadVideos = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-    .then((res) => res.json())
-    .then((data) => displayVideos(data.videos))
-    .catch((error) => console.log(error))
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error))
 };
 // displayVideos
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById("videos")
+    videoContainer.innerHTML = "";
+    // add no content section
+    if (videos.length == 0) {
+        videoContainer.classList.remove("grid")
+        videoContainer.innerHTML = `<div class="min-h-[300px] w-full flex flex-row gap-5 justify-center items-center">
+        <img src="./assets/Icon.png" alt="">
+        <h2 class="text-center text-xl font-bold">No Content Here In This Category</h2>
+    </div>`;
+    }else {
+        videoContainer.classList.add("grid")
+    }
 
+    // add full video section
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement("div");
         card.classList = "card card-compact"
-        card.innerHTML = 
-        `<figure class="h-[200px] relative">
+        card.innerHTML =
+            `<figure class="h-[200px] relative">
             <img
                 src=${video.thumbnail} class="h-full w-full object-cover" />
-            ${
-                video.others.posted_date?.length == 0 ? "" :  `<span class="absolute right-2 bottom-1 bg-slate-500 text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`
+            ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute right-2 bottom-1 bg-slate-500 text-sm text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`
             }    
                 
         </figure>
